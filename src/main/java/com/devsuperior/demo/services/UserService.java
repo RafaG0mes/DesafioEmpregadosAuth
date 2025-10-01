a3a90107401d01b7733574e18f1ae200e6346901
Rafael Gomes <gorraffa@hotmail.com>
2025-09-30 21:15:04 -0300

    user update

--- src/main/java/com/devsuperior/demo/services/UserService.java
+++ src/main/java/com/devsuperior/demo/services/UserService.java
@@ -1,11 +1,18 @@
 package com.devsuperior.demo.services;
 
+import com.devsuperior.demo.dto.RoleDTO;
 import com.devsuperior.demo.dto.UserDTO;
+import com.devsuperior.demo.dto.UserInsertDTO;
+import com.devsuperior.demo.dto.UserUpdateDTO;
+import com.devsuperior.demo.entities.Role;
 import com.devsuperior.demo.entities.User;
 import com.devsuperior.demo.repositories.RoleRepository;
 import com.devsuperior.demo.repositories.UserRepository;
+import com.devsuperior.demo.services.exceptions.DataBaseException;
 import com.devsuperior.demo.services.exceptions.ResourceNotFoundException;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -13,6 +20,7 @@ import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Propagation;
 import org.springframework.transaction.annotation.Transactional;
 
 import java.util.Optional;
@@ -40,6 +48,56 @@ public class UserService implements UserDetailsService {
         Optional<User> obj = userRepository.findById(id);
         User entity = obj.orElseThrow(() -> new ResourceNotFoundException("Entity not found"));
         return new UserDTO(entity);
+    }
+
+    @Transactional
+    public UserDTO insert(UserInsertDTO dto){
+        User entity = new User();
+        copyDtoToEntity(dto, entity);
+        entity.getRoles().clear();
+        Role role = roleRepository.findByAuthority("ROLE_OPERATOR");
+        entity.getRoles().add(role);
+        entity.setPassword(passwordEncoder.encode(dto.getPassword()));
+        entity = userRepository.save(entity);
+        return new UserDTO(entity);
+    }
+
+    @Transactional
+    public UserDTO update(Long id, UserUpdateDTO dto){
+        try {
+            User entity = userRepository.getReferenceById(id);
+            copyDtoToEntity(dto, entity);
+            entity = userRepository.save(entity);
+            return new UserDTO(entity);
+        }
+        catch (EntityNotFoundException e){
+            throw new ResourceNotFoundException("Id not found" + id);
+        }
+    }
+
+    @Transactional(propagation = Propagation.SUPPORTS)
+    public void delete(Long id){
+        if (!userRepository.existsById(id)){
+            throw new ResourceNotFoundException("Recurso não encontrado");
+        }
+        try {
+            userRepository.deleteById(id);
+        }
+        catch (DataIntegrityViolationException e){
+            throw new DataBaseException("Falha de integridade referênciada");
+        }
+    }
+
+    private void copyDtoToEntity(UserDTO dto, User entity){
+        entity.setFirstName(dto.getFirstName());
+        entity.setLastName(dto.getLastName());
+        entity.setEmail(dto.getEmail());
+
+        entity.getRoles().clear();
+        for (RoleDTO roleDTO : dto.getRoles()){
+            Role role = roleRepository.getReferenceById(roleDTO.getId());
+            entity.getRoles().add(role);
+        }
     }
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
